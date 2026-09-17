10a23dcc9130a560c7530969f0e3003738d3dd5b
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-17 22:15:49 +0500

    ✅ GSQ57 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/medium/GSQ57/solution.sql
@@ -0,0 +1,15 @@
+/* Solution as follows */
+
+CREATE TABLE customer
+(
+  Id INT PRIMARY KEY,
+  Name VARCHAR(30),
+  Age INT,
+  Address VARCHAR(30),
+  email VARCHAR(30) UNIQUE
+);
+INSERT INTO customer(Id,Name,Age,Address,email)
+VALUES  (1, 'John Smith', 25,  '123 Main St','john@example.com'),
+        (2, 'Sarah Johnson', 30,'456 Broadway','sarah@example.com'),
+        (3, 'Michael Brown', 45,  '789 5th Ave','michael@example.com'),
+        (4, 'Jessica Davis', 28,  '321 Elm St','vy@example.com');
