e4a0f3e664f6700f16ba6da589ab3269a06c5b35
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-17 22:14:08 +0500

    ✅ GSQ54 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/medium/GSQ54/solution.sql
+++ codechef/medium/GSQ54/solution.sql
@@ -1,19 +1,9 @@
 /* Solution as follows */
 
-CREATE TABLE customer
-(
-  Id INT,
-  Name TEXT,
-  Age INT,
-  Address TEXT
-);
-
-INSERT INTO customer(Id,Name,Age,Address)
-VALUES(1, 'John Smith', 25,  '123 Main St'),
-(2, 'Sarah Johnson', 30,'456 Broadway'),
-(3, 'Michael Brown', 45, '789 5th Ave'),
-(4, 'Jessica Davis', 28, '321 Elm St'),
-(5, 'David Lee', 35,  '555 Park Ave');
-
+ALTER TABLE customer
+ADD COLUMN email TEXT;
+INSERT INTO customer(Id,Name,Age,Address,email)
+VALUES(1, 'John Smith', 25,  'Main St','john@ex.com'),
+(2, 'Sarah Johnson', 30,'Broadway','sarah@ex.com');
 
 select  * from customer;
