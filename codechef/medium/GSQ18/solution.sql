9ef357705b737f83f4e7f2572579aadd83c7d310
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-18 21:32:05 +0500

    ✅ GSQ18 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-18

--- codechef/medium/GSQ18/solution.sql
+++ codechef/medium/GSQ18/solution.sql
@@ -1,4 +1,4 @@
-/* Write a query which does the following
-- Fetch the entry specifically from 2 columns - 'Passenger_name' and 'Gender' */
+/* Solution as follows */
 
-select Passenger_name,gender from flights
+select DISTINCT Origin 
+from Flights;
