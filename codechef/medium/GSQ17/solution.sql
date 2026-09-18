ef5fa9f0370ae41ece638130760657a86238d249
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-18 21:31:23 +0500

    ✅ GSQ17 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-18

--- codechef/medium/GSQ17/solution.sql
+++ codechef/medium/GSQ17/solution.sql
@@ -1,4 +1,1 @@
-/* Write a query which does the following
-- Fetch the entry specifically from 2 columns - 'Passenger_name' and 'Gender' */
-
-select Passenger_name,gender from flights
+select * from Flights
