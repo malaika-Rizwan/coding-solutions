d1e0c89ef019caebf37b9f8972307bc463e4da9c
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-20 21:18:32 +0500

    ✅ GSQ27 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-20

--- codechef/medium/GSQ27/solution.sql
+++ codechef/medium/GSQ27/solution.sql
@@ -1,8 +1,3 @@
-/* write a query with the following conditions
-- Destination city end in 'o' AND
-- Origin city starts with 'M' 
-*/
-
 select *from flights
-where Destination like '%o'
-and Origin like 'm%'
+where Gender='Female'
+and destination like'delhi'
