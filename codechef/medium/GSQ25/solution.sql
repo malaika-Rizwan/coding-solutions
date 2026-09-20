e359c9992f831d2b86ddcea7e267f38a3774bc0e
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-20 20:36:51 +0500

    ✅ GSQ25 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/medium/GSQ25/solution.sql
@@ -0,0 +1,6 @@
+/*write a query that returns the rows which meet the following conditions
+- Either Origin is Mumbai or the Destination is Mumbai */
+
+select *from flights
+where Origin='Mumbai'
+or Destination='Mumbai'
