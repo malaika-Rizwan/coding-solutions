967c38560e7c1994fcb1a41c94c0be01b02e21f7
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-20 20:33:57 +0500

    ✅ GSQ24 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/medium/GSQ24/solution.sql
@@ -0,0 +1,8 @@
+/* write a query below that returns the rows which meet the following conditions
+- Gender - Female
+- Destination - Cairo */
+
+select *from flights
+where Gender='Female'
+and Destination='Cairo'
+
