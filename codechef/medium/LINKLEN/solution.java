8d74942f740af270f27d2ff0f1d5e3ea54cef3f0
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-21 21:02:12 +0500

    ✅ LINKLEN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/LINKLEN/solution.java
@@ -0,0 +1,11 @@
+    public static int getLength(Node head) {
+        int count = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        return count;
+    }
