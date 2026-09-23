cd72a07a9b5cd72613e6bff0d5c0ac29babe9d8d
Malaika Rizwan <malaikarizwan121@gmail.com>
2026-09-23 22:22:22 +0500

    ✅ LJAAS10 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/LJAAS10/solution.java
+++ codechef/medium/LJAAS10/solution.java
@@ -1,9 +1,9 @@
-class Codechef
-{
-	public static void main (String[] args)
-	{
-        // Write your code here
-        System.out.println("learn coding on Codechef");
-      
-	}
+class Codechef {
+    public static void main(String[] args) {
+        int a=9;
+        int b=12;
+        int c=a*b;
+        
+        System.out.println(c);
+    }
 }
